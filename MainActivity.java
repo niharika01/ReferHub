c82ca681cc3e63165795b23da55225d925854f70
Niharika vijay pujar <33757482+niharika01@users.noreply.github.com>
2018-10-13 13:44:12 -0400

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,20 @@
 package e.niharikapujar.referhub;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] brands={"Uber","Bird","Lyft","UberEats","Audible"};
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(getListView().getContext(),android.R.layout.simple_list_item_1,brands);
+        getListView().setAdapter(adapter);
+
     }
 }
